a436f7ba7348108913e75ceb396856b1ea7749da
Mau! <mau.giachero@gmail.com>
2024-06-22 20:21:13 -0300

    creada carpeta routes y generado el router de productos con el metodo get

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hola!!')
+const productosRouter = require("./routes/productos.router");
+app.use("/productos", productosRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hola!!");
 });
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
