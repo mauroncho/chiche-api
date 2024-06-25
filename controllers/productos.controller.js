d7c5af8fa111d08d1a536d593fd88bd9be77e4e2
Mau! <mau.giachero@gmail.com>
2024-06-24 23:40:24 -0300

    obtererporid test

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,6 +1,6 @@
 const connect = require("../db/db");
 
-const metodo = (req, res) => {
+const listado = (req, res) => {
   const sql = "SELECT * FROM productos";
   connect.query(sql, (error, rows) => {
     if (error) {
@@ -13,24 +13,45 @@ const metodo = (req, res) => {
   });
 };
 
+const obtenerPorId = (req, res) => {
+  console.log(req.params);
+  const { cod_product } = req.params;
+  const sql = "SELECT * FROM productos WHERE cod_product = ?";
+  connect.query(sql, [cod_product], (error, rows) => {
+    if (error) {
+      console.error("Database query error:", error);
+      return res
+        .status(500)
+        .json({ error: "Intente más tarde", details: error.message });
+    }
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
+
+    res.json(rows);
+  });
+};
+
 const agregar = (req, res) => {
-  const {nombre, apellido, num_doc} = req.body;
-  
-  const sql = "INSERT INTO clientes (nombre, apellido, num_doc) VALUES (?, ?, ?)";
+  const { nombre, apellido, num_doc } = req.body;
+
+  const sql =
+    "INSERT INTO clientes (nombre, apellido, num_doc) VALUES (?, ?, ?)";
   connect.query(sql, [nombre, apellido, num_doc], (error, result) => {
-    console.log(result)
+    console.log(result);
     if (error) {
       return res.status(500).json({ error: "Intente más tarde" });
     }
 
-    const final = { ...req.body, id_clientes: result.insertId};
-   
+    const final = { ...req.body, id_clientes: result.insertId };
+
     res.status(201).json(final);
   });
 };
 
-
 module.exports = {
-  metodo,
+  listado,
   agregar,
+  obtenerPorId,
 };
--- routes/productos.router.js
+++ routes/productos.router.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/productos.controller");
-router.get("/", controller.metodo);
+router.get("/", controller.listado);
+router.get("/:id", controller.obtenerPorId);
 router.post("/", controller.agregar);
 
 module.exports = router;
